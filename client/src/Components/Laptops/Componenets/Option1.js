92c57e124a21339cba26db9462710a97ea733b63
kurogosane1 <magnite@hotmail.com>
2020-08-10 20:26:44 -0500

    solved the checked issue

--- client/src/Components/Laptops/Componenets/Option1.js
+++ client/src/Components/Laptops/Componenets/Option1.js
@@ -1,33 +1,16 @@
 import React, { useState } from "react";
 
 export default function Option1() {
-  const [ram, setRam] = useState([
-    {
-      id: 1,
-      description: "16gb 2333Mhz RAM",
-    },
-    {
-      id: 2,
-      description: "32gb 2333Mhz RAM",
-    },
-    {
-      id: 3,
-      description: "64gb 2333Mhz RAM",
-    },
-  ]);
-  const [style, setStyle] = useState({
-    style: "empty",
+  const [laptop, setLaptop] = useState({
+    Processor: "Core i7 14 core 3.0 ghz",
+    RAM: "16gb 2333Mhz RAM",
+    Storage: "1TB SSD",
+    GPU: "Nvidia RTX 3050 with 8GB of GDDR8 memory",
   });
 
-  // const handleClick = (e) => {
-  //   console.log(e.target.className);
-  //   console.log(e.target.value);
-  //   let value = e.target.value;
-
-  //   return value && style
-  //     ? (e.target.className = "blocked")
-  //     : (e.target.className = "empty");
-  // };
+  const handleClick = (e) => {
+    setLaptop({ RAM: e.target.value });
+  };
 
   const doThis = (e) => {
     e.preventDefault();
@@ -35,6 +18,7 @@ export default function Option1() {
   return (
     <div>
       <h2>This is input 1</h2>
+      <h2>{laptop.RAM}</h2>
       <form onSubmit={doThis}>
         <div className="Processor">
           {/* <input value="1" onClick={(e) => console.log(e.target.value + 1)}>
@@ -57,38 +41,46 @@ export default function Option1() {
           </label>
         </div>
         <div className="RAM">
-          <input
-            type="radio"
-            value="16gb 2333Mhz RAM"
-            name={1}
-            id={1}
-            // onClick={handleClick}
-            // className={style}
+          <label
+            className={laptop.RAM === "16gb 2333Mhz RAM" ? "blocked" : "empty"}
           >
-            {/* 16gb 2333Mhz RAM */}
-          </input>
+            16gb 2333Mhz RAM
+            <input
+              type="radio"
+              checked={laptop.RAM === "16gb 2333Mhz RAM"}
+              value="16gb 2333Mhz RAM"
+              name={1}
+              id={1}
+              onChange={handleClick}
+            />
+          </label>
 
-          <input
-            type="radio"
-            value="32gb 2333Mhz RAM"
-            name={2}
-            id={2}
-            // onClick={handleClick}
-            // className={style}
+          <label
+            className={laptop.RAM === "32gb 2333Mhz RAM" ? "blocked" : "empty"}
           >
-            {/* 32gb 2333Mhz RAM */}
-          </input>
-{/* 
-          <input
-            type="radio"
-            value="64gb 2333Mhz RAM"
-            name={3}
-            id={3}
-            // onClick={handleClick}
-            // className={style}
+            32gb 2333Mhz RAM
+            <input
+              type="radio"
+              checked={laptop.RAM === "32gb 2333Mhz RAM"}
+              value="32gb 2333Mhz RAM"
+              name={2}
+              id={2}
+              onChange={handleClick}
+            />
+          </label>
+          <label
+            className={laptop.RAM === "64gb 2333Mhz RAM" ? "blocked" : "empty"}
           >
-            64gb 2333Mhz RAM
-          </input> */}
+            64gb 2333Mhz RAM{" "}
+            <input
+              type="radio"
+              checked={laptop.RAM === "64gb 2333Mhz RAM"}
+              value="64gb 2333Mhz RAM"
+              name={3}
+              id={3}
+              onChange={handleClick}
+            />
+          </label>
         </div>
         <div className="Storage">
           <label>
