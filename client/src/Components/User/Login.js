52fe7097439d3175579b1635605237bf0e5b998c
kurogosane1 <magnite@hotmail.com>
2020-08-13 17:22:22 -0500

    created the user login in form

--- client/src/Components/User/Login.js
+++ client/src/Components/User/Login.js
@@ -1,12 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 // import { useLocation } from "react-router-dom";
 
 export default function Login(props) {
-  //   const location = useLocation();
+  const [user, setUser] = useState({});
+
+  const handleChange = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
+  const loginAccess = (e) => {
+    
+  };
 
   return (
     <div>
-      <h2>this is to redirect to login page</h2>
+      <label>Please sign in</label>
+      <form onSubmit={loginAccess}>
+        <div>
+          <input
+            type="email"
+            name="Deepin-id"
+            placeholder="Deepin Id"
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <input
+            type="password"
+            name="Deepin-password"
+            placeholder="Password"
+            onChange={handleChange}
+          />
+        </div>
+        <br />
+        <div>
+          <span>
+            Your Deepin ID is the email address you use to sign in to the App
+            Store
+          </span>
+        </div>
+        <div>
+          <input type="submit" value="Sign In" />
+        </div>
+      </form>
     </div>
   );
 }
